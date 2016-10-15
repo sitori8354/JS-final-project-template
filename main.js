3f277133d21692f77e420b33db81b9340859e90f
sitori8354 <kmes9940723@gmail.com>
2016-10-15 17:09:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ var FPS=60;
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
 var enemyImg=document.createElement("img");
-bglmg
+bglmg.src="images/slime.png";
 
 
 
 
 function draw(){
   ctx.drawImage(bglmg,0,0);
+  ctx.drawImage(enemyImg,0,0);
 }
 setInterval(draw,16);
   
