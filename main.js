2bed0824bbce0b45aa5b96e590ff193868e890cb
sitori8354 <kmes9940723@gmail.com>
2016-10-15 17:01:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+var canvas=document.getElementByld("game-canvas");
+var ctx=canvas.getContext("2d");
+
 
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
-var canvas=document.getElementByld("game-canvas");
-var ctx=canvas.getContext("2d");
+
+
 
 
 function draw(){
