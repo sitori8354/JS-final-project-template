197526ed3e789cb05fceb04d532a5a65ebcdd973
sitori8354 <kmes9940723@gmail.com>
2016-10-29 16:34:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -67,11 +67,8 @@ var cursor={};
 var towerShow={};
 $("#game-canvas").on("click",function(){
   if (iscoll(cursor.x,cursor.y,bin.x,bin.y,100,100)){
-    if(isBuild){
-      isBuild=false;
-  }else{
-    isBuild=true;}
-  }else{isBuild=false;}
+    if(isBuild){isBuild=false;}
+    else{isBuild=true;}else{isBuild=false;}
   if (isBuild&&iscoll(cursor.x,cursor.y)){
   towerShow.x=cursor.x;
   towerShow.y=cursor.y;
