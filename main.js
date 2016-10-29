ea548fdd98f6e5f4fd55b49d50ae0ff73270c6a7
sitori8354 <kmes9940723@gmail.com>
2016-10-29 17:13:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ var FPS=60;
 ///畫上基本圖像
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
-var enemyImg=document.createElement("img");
-enemyImg.src="images/slime.gif";
-var person1=document.createElement("img");
-person1.src="images/daigh.gif";
-var person2=document.createElement("img");
-person2.src="images/jason.gif";
+var slimeImg=document.createElement("img");
+slimeImg.src="images/slime.gif";
+//var person1=document.createElement("img");
+//person1.src="images/daigh.gif";
+//var person2=document.createElement("img");
+//person2.src="images/jason.gif";
 var tower=document.createElement("img");
 tower.src="images/tower.png";
 var towerbtn=document.createElement("img");
 towerbtn.src="images/tower-btn.png";
-var shoot=document.createElement("img");
-shoot.src="images/cannon-ball.png";
-var crosshair=document.createElement("img");
-crosshair.src="images/crosshair.png";
-var person3=document.createElement("img");
-person3.src="images/rukia.gif";
+//var shoot=document.createElement("img");
+//shoot.src="images/cannon-ball.png";
+//var crosshair=document.createElement("img");
+//crosshair.src="images/crosshair.png";
+//var person3=document.createElement("img");
+//person3.src="images/rukia.gif";
 
 ///設定圖像位置
 var enemy={
@@ -79,3 +79,5 @@ function iscoll(pointX,pointY,targetX,targetY,targetWidth,targetHeight){
     return true;}else{
       return false;}
 }
+//設定敵人
+
